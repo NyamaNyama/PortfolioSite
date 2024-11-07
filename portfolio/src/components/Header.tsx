bef956544f82efea804231cc901d783e5115a9f3
Nyama N <tihon31.01.04@mail.ru>
2024-11-07 21:09:59 +1000

    Add Header and Header style

---
+++ portfolio/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "../styles/Header.css"
+//import {NavLink} from "react-router-dom"
+
+export const Header = () => {
+    return (
+    <header className="header">
+        <div className="top-block"></div>
+        <div className="bottom-block"></div>
+    </header>
+    );
+};
