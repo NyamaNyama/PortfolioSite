485eccb1281adcfc48776cab842fb1291278227c
Nyama N <tihon31.01.04@mail.ru>
2024-11-08 19:26:31 +1000

    Add pages files and install react-router-dom

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -1,11 +1,17 @@
 import "../styles/Header.css"
-//import {NavLink} from "react-router-dom"
+import { Link } from 'react-router-dom';
+
 
 export const Header = () => {
     return (
     <header className="header">
-        <div className="top-block"></div>
-        <div className="bottom-block"></div>
+        <nav className="nav">
+        <Link to="/" className="nav-link">Main</Link>
+        <Link to="/about" className="nav-link">About</Link>
+        <Link to="/skills" className="nav-link">Technologies</Link>
+        <Link to="/projects" className="nav-link">Projects</Link>
+        <Link to="/contact" className="nav-link">Contact</Link>
+      </nav>
     </header>
     );
 };
