00c71d0870e7b2552c5cec2e300646c0c9d1aae2
Nyama N <tihon31.01.04@mail.ru>
2024-11-18 21:34:55 +1000

    removed selection and dragging of navigation

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -3,17 +3,20 @@ import { Link } from 'react-router-dom';
 
 
 export const Header = () => {
+    const handleDragStart = (event: React.DragEvent<HTMLAnchorElement>) => {
+        event.preventDefault(); 
+      };
     return (
     <header className="header">
         <div className="company-name">
             <h1>ITxON</h1>
         </div>
         <nav className="navigation">
-        <Link to="/" className="nav-link">Главная</Link>
-        <Link to="/about" className="nav-link">Обо мне</Link>
-        <Link to="/skills" className="nav-link">Технологии</Link>
-        <Link to="/projects" className="nav-link">Проекты</Link>
-        <Link to="/contact" className="nav-link">Обратная связь</Link>
+        <Link to="/" className="nav-link" onDragStart={handleDragStart}>Главная</Link>
+        <Link to="/about" className="nav-link" onDragStart={handleDragStart}>Обо мне</Link>
+        <Link to="/skills" className="nav-link" onDragStart={handleDragStart}>Навыки</Link>
+        <Link to="/projects" className="nav-link" onDragStart={handleDragStart}>Проекты</Link>
+        <Link to="/contact" className="nav-link" onDragStart={handleDragStart}>Обратная связь</Link>
         </nav>
     </header>
     );
