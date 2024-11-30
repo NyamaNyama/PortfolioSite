ad0f482b636d205dc661b485ddb863d765ddd5a6
Nyama N <tihon31.01.04@mail.ru>
2024-11-30 12:09:00 +1000

    Add style and input fields for Contact page

--- portfolio/src/pages/Contactpage.tsx
+++ portfolio/src/pages/Contactpage.tsx
@@ -1,9 +1,53 @@
+import { useState } from "react";
 import { Layout } from "../components/Layout";
+import "../styles/Contact.css"
 
 export const Contact = () =>{
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        alert("Сообщение отправлено!");
+        setName("");
+        setEmail("");
+        setMessage("");
+    };
     return (
         <Layout>
-        <h1>Это страница для обратной связи со мной. Её я тоже сделаю в лабораторной 2</h1>
+        <div className="contacts-container">
+            <h2>Свяжитесь со мной</h2>
+            <form className="contact-form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">Имя:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                <label htmlFor="message">Сообщение:</label>
+                <textarea
+                    id="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                </div>
+                <button type="submit">Отправить</button>
+            </form>
+        </div>
         </Layout>
     );
 };
