a3aba69897290169a8cd3360ffd1abc36c1a9b57
Nyama N <tihon31.01.04@mail.ru>
2024-12-13 17:20:12 +1000

    Add strict typing for preloadedState and validation of data from localStorage

--- portfolio/src/store/index.ts
+++ portfolio/src/store/index.ts
@@ -1,9 +1,41 @@
 import { configureStore } from '@reduxjs/toolkit';
 import projectsReducer from './projectsSlice';
+import { IProject } from '../types/Project';
 
-const preloadedState = {
+type PreloadedState = {
     projects: {
-        items: JSON.parse(localStorage.getItem('projects') || '[]'),
+      items: IProject[];
+    };
+};
+
+const isValidProject = (project: IProject) => {
+    return (
+      typeof project.id === 'number' &&
+      typeof project.title === 'string' &&
+      typeof project.description === 'string' &&
+      project.technologies.every((tech: unknown) => typeof tech === 'string') &&
+      typeof project.link === 'string'
+    );
+};
+  
+const loadProjectsFromLocalStorage = (): PreloadedState['projects']['items'] => {
+    try {
+      const storedProjects = localStorage.getItem('projects');
+      if (storedProjects) {
+        const parsedProjects = JSON.parse(storedProjects);
+        if (Array.isArray(parsedProjects)) {
+          return parsedProjects.filter(isValidProject);
+        }
+      }
+    } catch (error) {
+      console.error('Ошибка при чтении данных из localStorage:', error);
+    }
+    return [];
+};
+
+const preloadedState: PreloadedState = {
+    projects: {
+      items: loadProjectsFromLocalStorage(),
     },
 };
 
@@ -15,8 +47,12 @@ export const store = configureStore({
 });
 
 store.subscribe(() => {
-    localStorage.setItem('projects', 
-    JSON.stringify(store.getState().projects.items));
+    try {
+        localStorage.setItem('projects', JSON.stringify(store.getState().projects.items));
+    } 
+    catch (error) {
+        console.error('Ошибка при сохранении данных в localStorage:', error);
+    }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
