1875543a92a9c25d62c6ed22070ca914f6ffd2a0
Nyama N <tihon31.01.04@mail.ru>
2025-01-27 12:37:34 +1000

    Add ThemeContext

---
+++ portfolio/src/context/ThemeContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, useState, ReactNode, useContext } from 'react';
+
+type Theme = 'light' | 'dark';
+
+interface ThemeContextProps {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps>({
+  theme: 'light',
+  toggleTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }: { children: ReactNode } ) => {
+  const [theme, setTheme] = useState<Theme>('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
