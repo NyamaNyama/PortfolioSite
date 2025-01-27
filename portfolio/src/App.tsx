90fb6f2ce28f00bf6212a679050f7c8208fcd630
Nyama N <tihon31.01.04@mail.ru>
2025-01-27 13:18:54 +1000

    Connect theme for App

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -7,18 +7,20 @@ import {Projects} from "./pages/Projectspage"
 import {Skills} from "./pages/Skillspage"
 import { Footer } from "./components/Footer"
 import { socialLinks } from "./data/SocialLinks"
+import { useTheme } from './context/ThemeContext'
 export function App() {
   
+  const { theme } = useTheme();
 
   return (
-    <div>
+    <div className={`app ${theme}`}>
       <Header/>
       <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/skills" element={<Skills/>} />
-      <Route path="/projects" element={<Projects/>} />
-      <Route path="/contact" element={<Contact/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/skills" element={<Skills/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
       <Footer socialLinks={socialLinks} email ="tihonshkarin31012004@gmail.com"/>
     </div>
